a2de14311422fb6bf10e2660e604542c72f921b4
Raymond Loman <raymond@carpago.nl>
2018-06-13 15:11:36 +0200

    Add demo for Raymond regarding stores

--- src/main/java/com/capgemini/guitar/store/Store.java
+++ src/main/java/com/capgemini/guitar/store/Store.java
@@ -7,9 +7,32 @@ import java.util.Map;
 
 public class Store {
 
-    private  Map<Integer, Guitar> store = new HashMap<>();
+    private static int lastGuestNumber = 0;
+
+    // key : integer, value : Room (or subtype)
+    private Map<Integer, Room> roomHashMap = new HashMap<>();
+
+    private Map<Integer, Guest> guestStore = new HashMap<>();
 
     public void add(int i, Guitar g) {
-        this.store.put(i, g);
+        this.roomHashMap.put(i, g);
     }
+
+    public void add(int i, Room r) {
+        this.roomHashMap.put(i, r);
+    }
+
+    public void add(Guest g) {
+        this.guestStore.put(++lastGuestNumber, g);
+
+    }
+}
+
+
+class Room {
+
+}
+
+class Guest {
+
 }
